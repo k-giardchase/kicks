796e4cd92596be21a5b76e8154a6cf1659514fac
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-03-27 10:41:28 -0700

    Add test for getAll method

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -82,5 +82,25 @@
            $result = Brand::getAll();
            $this->assertEquals([$test_brand], $result);
        }
+
+       function test_getAll()
+       {
+           //Arrange
+           $brand_name = 'Kicks';
+           $id = 1;
+           $test_brand = new Brand($brand_name, $id);
+           $test_brand->save();
+
+           $brand_name2 = 'Converse';
+           $id2 = 2;
+           $test_brand2 = new Brand($brand_name2, $id2);
+           $test_brand2->save();
+
+           //Act
+           $result = Brand::getAll();
+
+           //Assert
+           $this->assertEquals([$test_brand, $test_brand2], $result);
+       }
    }
 ?>
