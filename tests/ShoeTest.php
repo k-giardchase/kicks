307ac4fb338df609f8bad8a647a3cecfd43ee749
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-03-27 09:13:34 -0700

    Create ShoeTest.php file

---
+++ tests/ShoeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+    $DB = new PDO('pgsql:host=localhost;dbname=shoes_test');
+
+    /**
+   * @backupGlobals disabled
+   * $backupStaticAttribute disabled
+   */
+
+   require_once 'src/Shoe.php';
+
+   class ShoeTest extends PHPUnit_Framework_TestCase
+   {
+
+       
+
+
+
+   }
+
+
+
+?>
