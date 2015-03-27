5f59664930224b6506649de9187f6b0244a3656e
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-03-27 10:50:25 -0700

    Add test for find method

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -128,5 +128,26 @@
            $result = Brand::getAll();
            $this->assertEquals([], $result);
        }
+
+       function test_find()
+       {
+           //Arrange
+           $brand_name = 'Kicks';
+           $id = 1;
+           $test_brand = new Brand($brand_name, $id);
+           $test_brand->save();
+
+           $brand_name2 = 'Converse';
+           $id2 = 2;
+           $test_brand2 = new Brand($brand_name2, $id2);
+           $test_brand2->save();
+
+           //Act
+           Brand::find($brand_name2->getId());
+
+           //Assert
+           $result = $brand_name2
+           $this->assertEquals($brand_name2, $result);
+       }
    }
 ?>
