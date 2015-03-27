b8b7c7480176b8f8acc30f063891de58516fe60b
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-03-27 15:53:15 -0700

    Dump databases

---
+++ shoes_test.sql
@@ -0,0 +1,268 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: brands; Type: TABLE; Schema: public; Owner: Kyle; Tablespace: 
+--
+
+CREATE TABLE brands (
+    id integer NOT NULL,
+    brand_name character varying
+);
+
+
+ALTER TABLE brands OWNER TO "Kyle";
+
+--
+-- Name: brands_id_seq; Type: SEQUENCE; Schema: public; Owner: Kyle
+--
+
+CREATE SEQUENCE brands_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE brands_id_seq OWNER TO "Kyle";
+
+--
+-- Name: brands_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Kyle
+--
+
+ALTER SEQUENCE brands_id_seq OWNED BY brands.id;
+
+
+--
+-- Name: stores; Type: TABLE; Schema: public; Owner: Kyle; Tablespace: 
+--
+
+CREATE TABLE stores (
+    id integer NOT NULL,
+    name character varying
+);
+
+
+ALTER TABLE stores OWNER TO "Kyle";
+
+--
+-- Name: stores_brands; Type: TABLE; Schema: public; Owner: Kyle; Tablespace: 
+--
+
+CREATE TABLE stores_brands (
+    id integer NOT NULL,
+    store_id integer,
+    brand_id integer
+);
+
+
+ALTER TABLE stores_brands OWNER TO "Kyle";
+
+--
+-- Name: stores_brands_id_seq; Type: SEQUENCE; Schema: public; Owner: Kyle
+--
+
+CREATE SEQUENCE stores_brands_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE stores_brands_id_seq OWNER TO "Kyle";
+
+--
+-- Name: stores_brands_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Kyle
+--
+
+ALTER SEQUENCE stores_brands_id_seq OWNED BY stores_brands.id;
+
+
+--
+-- Name: stores_id_seq; Type: SEQUENCE; Schema: public; Owner: Kyle
+--
+
+CREATE SEQUENCE stores_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE stores_id_seq OWNER TO "Kyle";
+
+--
+-- Name: stores_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Kyle
+--
+
+ALTER SEQUENCE stores_id_seq OWNED BY stores.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Kyle
+--
+
+ALTER TABLE ONLY brands ALTER COLUMN id SET DEFAULT nextval('brands_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Kyle
+--
+
+ALTER TABLE ONLY stores ALTER COLUMN id SET DEFAULT nextval('stores_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Kyle
+--
+
+ALTER TABLE ONLY stores_brands ALTER COLUMN id SET DEFAULT nextval('stores_brands_id_seq'::regclass);
+
+
+--
+-- Data for Name: brands; Type: TABLE DATA; Schema: public; Owner: Kyle
+--
+
+COPY brands (id, brand_name) FROM stdin;
+\.
+
+
+--
+-- Name: brands_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Kyle
+--
+
+SELECT pg_catalog.setval('brands_id_seq', 351, true);
+
+
+--
+-- Data for Name: stores; Type: TABLE DATA; Schema: public; Owner: Kyle
+--
+
+COPY stores (id, name) FROM stdin;
+\.
+
+
+--
+-- Data for Name: stores_brands; Type: TABLE DATA; Schema: public; Owner: Kyle
+--
+
+COPY stores_brands (id, store_id, brand_id) FROM stdin;
+61	516	265
+62	517	266
+63	518	266
+64	531	277
+65	532	278
+66	533	278
+67	545	287
+68	546	288
+69	547	288
+70	560	299
+71	561	300
+72	562	300
+73	575	311
+74	576	312
+75	577	312
+76	590	323
+77	591	324
+78	592	324
+79	603	325
+80	604	326
+81	604	327
+82	605	335
+83	606	336
+84	607	336
+85	618	337
+86	619	338
+87	619	339
+88	620	347
+89	621	348
+90	622	348
+91	633	349
+92	634	350
+93	634	351
+\.
+
+
+--
+-- Name: stores_brands_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Kyle
+--
+
+SELECT pg_catalog.setval('stores_brands_id_seq', 93, true);
+
+
+--
+-- Name: stores_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Kyle
+--
+
+SELECT pg_catalog.setval('stores_id_seq', 634, true);
+
+
+--
+-- Name: brands_pkey; Type: CONSTRAINT; Schema: public; Owner: Kyle; Tablespace: 
+--
+
+ALTER TABLE ONLY brands
+    ADD CONSTRAINT brands_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: stores_brands_pkey; Type: CONSTRAINT; Schema: public; Owner: Kyle; Tablespace: 
+--
+
+ALTER TABLE ONLY stores_brands
+    ADD CONSTRAINT stores_brands_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: stores_pkey; Type: CONSTRAINT; Schema: public; Owner: Kyle; Tablespace: 
+--
+
+ALTER TABLE ONLY stores
+    ADD CONSTRAINT stores_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: Kyle
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM "Kyle";
+GRANT ALL ON SCHEMA public TO "Kyle";
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
