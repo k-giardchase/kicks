39ee795f1e5766af9ed6ff2026d2a942f8a4f1cf
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-03-31 21:03:04 -0700

    change name of databasestores_brands to brands_stores in store.php

--- src/Store.php
+++ src/Store.php
@@ -47,7 +47,7 @@
         function delete()
         {
             $GLOBALS['DB']->exec("DELETE FROM stores WHERE id = {$this->getId()};");
-            $GLOBALS['DB']->exec("DELETE FROM stores_brands WHERE store_id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM brands_stores WHERE store_id = {$this->getId()};");
         }
 
         static function find($search_id)
@@ -85,12 +85,12 @@
 
         function addBrand($new_brand)
         {
-            $GLOBALS['DB']->exec("INSERT INTO stores_brands (store_id, brand_id) VALUES ({$this->getId()}, {$new_brand->getId()});");
+            $GLOBALS['DB']->exec("INSERT INTO brands_stores (store_id, brand_id) VALUES ({$this->getId()}, {$new_brand->getId()});");
         }
 
         function getBrands()
         {
-            $query = $GLOBALS['DB']->query("SELECT brands.* FROM stores JOIN stores_brands ON (stores.id = stores_brands.store_id) JOIN brands ON (stores_brands.brand_id = brands.id) WHERE stores.id = {$this->getId()};");
+            $query = $GLOBALS['DB']->query("SELECT brands.* FROM stores JOIN brands_stores ON (stores.id = brands_stores.store_id) JOIN brands ON (brands_stores.brand_id = brands.id) WHERE stores.id = {$this->getId()};");
            $returned_brands = $query->fetchAll(PDO::FETCH_ASSOC);
                $brands = array();
                foreach($returned_brands as $returned_brand) {
